88ab14e0415250de4ef37f5435d6ab2dba00516a
Abrarsaadxv <abrarsaadd1@gmail.com>
2024-03-27 23:38:13 -1200

    hM

--- src/test/java/stepdefinitions/HW/homework1Day2.java
+++ src/test/java/stepdefinitions/HW/homework1Day2.java
@@ -3,14 +3,22 @@ package stepdefinitions.HW;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
 import utilities.Driver;
 
 
 public class homework1Day2 {
     @Given("the user navigates to the {string} page")
     public void theUserNavigatesToThePage(String url) {
-        Driver.getDriver().get(url);
-        System.out.println("Navigating to page: " + url);
+        Driver.getDriver().get("https:www.google.com");
+           Driver.getDriver().findElement(By.name("q")).sendKeys(url+Keys.ENTER);
+
+
+
+//         System.out.println("Navigating to page: " + url);
 
     }
 
@@ -22,6 +30,7 @@ public class homework1Day2 {
 
     @And("the user verifies that the page contains {string}")
     public void theUserVerifiesThatThePageContains(String arg0) {
+//        Assert.assertTrue();
     }
 
     @And("the user close the page")
